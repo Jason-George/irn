ecdc7b796661e79524e530487183e59ba7379074
Jason Christie <55175444+Jason-George@users.noreply.github.com>
2021-03-08 12:01:13 +0530

    Update resnet50_cam.py

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -6,10 +6,11 @@ from net import resnet50
 
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self,n_labels):
         super(Net, self).__init__()
 
         self.resnet50 = resnet50.resnet50(pretrained=True, strides=(2, 2, 2, 1))
+        self.n_labels = n_labels
 
         self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
                                     self.resnet50.layer1)
@@ -17,7 +18,7 @@ class Net(nn.Module):
         self.stage3 = nn.Sequential(self.resnet50.layer3)
         self.stage4 = nn.Sequential(self.resnet50.layer4)
 
-        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
+        self.classifier = nn.Conv2d(2048, self.n_labels, 1, bias=False)
 
         self.backbone = nn.ModuleList([self.stage1, self.stage2, self.stage3, self.stage4])
         self.newly_added = nn.ModuleList([self.classifier])
@@ -32,7 +33,7 @@ class Net(nn.Module):
 
         x = torchutils.gap2d(x, keepdims=True)
         x = self.classifier(x)
-        x = x.view(-1, 20)
+        x = x.view(-1, self.n_labels)
 
         return x
 
