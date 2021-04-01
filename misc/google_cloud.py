d5abe03abc52305cdb15829a6f7de076196d2e77
Jason Christie <55175444+Jason-George@users.noreply.github.com>
2021-04-02 01:02:15 +0530

    Update google_cloud.py

--- misc/google_cloud.py
+++ misc/google_cloud.py
@@ -9,3 +9,12 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     print('File {} uploaded to {}.'.format(
         source_file_name,
         destination_blob_name))
+
+def download_to_kaggle(bucket_name,destination_directory,file_name,prefix=''):
+    """Takes the data from your GCS Bucket and puts it into the working directory of your Kaggle notebook"""
+    storage_client = storage.Client('liquid-force-295404')
+    os.makedirs(destination_directory, exist_ok = True)
+    full_file_path = os.path.join(destination_directory, file_name)
+    blobs = storage_client.list_blobs(bucket_name,prefix=prefix)
+    for blob in blobs:
+        blob.download_to_filename(full_file_path)
