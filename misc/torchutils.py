937549027db22c7e71a4ca7d3b928da6e2d2cee0
Jason Christie <55175444+Jason-George@users.noreply.github.com>
2021-04-01 12:21:47 +0530

    Update torchutils.py

--- misc/torchutils.py
+++ misc/torchutils.py
@@ -74,3 +74,20 @@ def gap2d(x, keepdims=False):
         out = out.view(out.size(0), out.size(1), 1, 1)
 
     return out
+
+def load_checkpoint(model, optimizer, filename='checkpoint.pth.tar'):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        #losslogger = checkpoint['losslogger']
+        print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer
